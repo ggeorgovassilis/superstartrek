f893c5fe78544596cf21a2c70a7efdd87a89458f
george georgovassilis <g.georgovassilis@gmail.com>
2019-01-20 17:24:42 +0100

    test gwt

---
+++ src/main/java/superstartrek/client/Application.java
@@ -0,0 +1,12 @@
+package superstartrek.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class Application implements EntryPoint{
+
+	public void onModuleLoad() {
+		Window.alert("hi");
+	}
+
+}
