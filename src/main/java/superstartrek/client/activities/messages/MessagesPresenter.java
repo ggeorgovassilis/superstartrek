1f7fa2dc39588ffba0db0b8b5ba40b1b90a291cb
george georgovassilis <g.georgovassilis@gmail.com>
2019-03-24 09:37:08 +0100

    hiding messages async (trying to workaround chrome mobile issue), deployment

--- src/main/java/superstartrek/client/activities/messages/MessagesPresenter.java
+++ src/main/java/superstartrek/client/activities/messages/MessagesPresenter.java
@@ -1,12 +1,16 @@
 package superstartrek.client.activities.messages;
 
+import javax.swing.text.Utilities;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 
 import superstartrek.client.Application;
 import superstartrek.client.activities.BasePresenter;
 import superstartrek.client.activities.glasspanel.GlassPanelEvent;
 import superstartrek.client.activities.glasspanel.GlassPanelEvent.Action;
 import superstartrek.client.activities.glasspanel.GlassPanelHandler;
+import superstartrek.client.utils.Timer;
 
 public class MessagesPresenter extends BasePresenter<MessageActivity> implements MessageHandler, GlassPanelHandler {
 
@@ -18,6 +22,7 @@ public class MessagesPresenter extends BasePresenter<MessageActivity> implements
 
 	@Override
 	public void messagePosted(String formattedMessage, String category) {
+		GWT.log("message posted "+formattedMessage);
 		((MessagesView) getView()).showMessage(formattedMessage, category);
 		application.events.fireEvent(new GlassPanelEvent(Action.show));
 		getView().show();
@@ -26,10 +31,21 @@ public class MessagesPresenter extends BasePresenter<MessageActivity> implements
 	public void dismissButtonClicked() {
 		hideMessages();
 	}
-
+	
 	public void hideMessages() {
+		GWT.log("Hide messages async "+getView().isVisible());
 		if (!getView().isVisible())
 			return;
+		Timer.postpone(new ScheduledCommand() {
+			@Override
+			public void execute() {
+				hideMessagesNow();
+			}
+		});
+	}
+	
+	public void hideMessagesNow() {
+		GWT.log("Hide messages now "+getView().isVisible());
 		getView().hide();
 		((MessagesView) getView()).clear();
 		application.events.fireEvent(new MessageEvent(MessageEvent.Action.hide, null, null));
@@ -42,11 +58,13 @@ public class MessagesPresenter extends BasePresenter<MessageActivity> implements
 
 	@Override
 	public void glassPanelHidden() {
+		GWT.log("glass panel hidden");
 		hideMessages();
 	}
 
 	@Override
 	public void glassPanelClicked() {
+		GWT.log("glass panel clicked");
 		hideMessages();
 	}
 
