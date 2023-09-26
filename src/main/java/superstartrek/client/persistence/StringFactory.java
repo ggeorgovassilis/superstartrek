136120c5de9bb213d4b84eb876bc94a7212f353c
George Georgovassilis <g.georgovassilis@gmail.com>
2023-09-26 09:56:26 +0300

    Fixed #100

---
+++ src/main/java/superstartrek/client/persistence/StringFactory.java
@@ -0,0 +1,59 @@
+package superstartrek.client.persistence;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NodeList;
+
+public class StringFactory {
+
+	// Since the StarMap is fixed size, there's an upper limit of objects in it
+	String[] arr=new String[8000];
+	int index = 0;
+	int length;
+	String lastString = "";
+	
+	public StringFactory append(String s) {
+		arr[index++] = s;
+		lastString = s;
+		length+=s.length();
+		return this;
+	}
+	
+	public StringFactory append(int i) {
+		return append(Integer.toString(i));
+	}
+
+	public StringFactory append(boolean b) {
+		return append(Boolean.toString(b));
+	}
+
+	int length() {
+		return length;
+	}
+	
+	public char getLastCharacter() {
+		return lastString.charAt(lastString.length()-1);
+	}
+	
+	public StringFactory deleteLastCharacter() {
+		lastString = lastString.substring(0, lastString.length()-1);
+		arr[index-1] = lastString;
+		length--;
+		return this;
+	}
+	
+	
+	
+	//@formatter:off
+	private final native String _toString() /*-{ 
+		return this.@superstartrek.client.persistence.StringFactory::arr.join("");
+	}-*/;
+	//@formatter:on
+
+	@Override
+	public String toString() {
+		if (GWT.isClient())
+			return _toString();
+		else return String.join("", arr);
+	}
+}
