c1e8b5498e90c25848c3162eb888bd53524ae207
George Georgovassilis <g.georgovassilis@gmail.com>
2024-05-01 10:14:27 +0300

    fixed warning, fixed TODO

--- src/main/java/superstartrek/client/eventbus/EventsMixin.java
+++ src/main/java/superstartrek/client/eventbus/EventsMixin.java
@@ -12,14 +12,16 @@ public interface EventsMixin {
 		getEvents().fireEvent(type, callback);
 	}
 
-	//TODO: all invocations I could find are addHandler(Event, this). Can the "handler" parameter
-	//be omitted?
 	default <T extends EventHandler> void addHandler(Event<T> type) {
-		getEvents().addHandler(type, (T)this);
+		@SuppressWarnings("unchecked")
+		T handler = (T)this;
+		getEvents().addHandler(type, handler);
 	}
 
 	default <T extends EventHandler> void removeHandler(Event<T> type) {
-		getEvents().removeHandler(type, (T)this);
+		@SuppressWarnings("unchecked")
+		T handler = (T)this;
+		getEvents().removeHandler(type, handler);
 	}
 
 	default <T extends EventHandler> void removeHandler(T handler) {
