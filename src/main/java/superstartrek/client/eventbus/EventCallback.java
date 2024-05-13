3fc8f6ce1b889f135eeaaf954c96721d1c3002af
George Georgovassilis <g.georgovassilis@gmail.com>
2024-05-13 17:25:24 +0300

    documentation

--- src/main/java/superstartrek/client/eventbus/EventCallback.java
+++ src/main/java/superstartrek/client/eventbus/EventCallback.java
@@ -1,5 +1,11 @@
 package superstartrek.client.eventbus;
 
+/**
+ * DO NOT DELETE. 
+ * Handler interface for events. A static code analysis will show only one use in {@link EventBus#fireEvent(Event, EventCallback)} 
+ * which is wrong because we do lambdas everywhere (single method interfaces) which obscures all other uses.
+ * @param <T> T can't be more specific because it is whatever the callback handler needs.
+ */
 public interface EventCallback<T> {
 
 	void call(T arg);
