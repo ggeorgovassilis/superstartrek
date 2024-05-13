7e26bc6c94e72d21f69ce04f7e376a5cfd1436a0
George Georgovassilis <g.georgovassilis@gmail.com>
2024-05-13 15:19:11 +0300

    Spelling

--- src/main/java/superstartrek/client/activities/messages/MessagesViewImpl.java
+++ src/main/java/superstartrek/client/activities/messages/MessagesViewImpl.java
@@ -48,7 +48,7 @@ public class MessagesViewImpl extends PopupViewImpl<MessagesPresenter> implement
 		inTransition = true;
 		//Most (all?) messages arrive within the same slot between user interactions on the main js thread.
 		//Modifying the view with messages during the popup animation would slow it down considerably.
-		//Deferring the apparence ensures all DOM modifications have finished before the animation.
+		//Deferring the appearance ensures all DOM modifications have finished before the animation.
 		superstartrek.client.utils.Timer.postpone(()->{
 			super.show();
 			inTransition = false;
